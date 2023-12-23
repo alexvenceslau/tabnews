d23bbecd40edad197c4fcd11af6447a3325a75af
alexvenceslau <alexvcosta.com@gmail.com>
2023-12-23 00:16:58 -0300

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,59 @@
 import database from "infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
-  response.status(200).json({ chave: "açentuação" });
+  try {
+    const firstQueryTimer = performance.now();
+
+    const [maxConnectionsResult, superuserReservedConnectionsResult] =
+      await database.query(
+        "SHOW max_connections; SHOW superuser_reserved_connections; "
+      );
+    const firstQueryDuration = performance.now() - firstQueryTimer;
+
+    const maxConnetionsValue = maxConnectionsResult.rows[0].max_connections;
+    const maxConnetionsReservedValue =
+      superuserReservedConnectionsResult.rows[0].superuser_reserved_connections;
+    const maxConnections = maxConnetionsValue - maxConnetionsReservedValue;
+
+    const secondQueryTimer = performance.now();
+    const openConnectionsResult = await database.query({
+      text: "SELECT numbackends as opened_connections FROM pg_stat_database WHERE datname = $1",
+      values: [process.env.POSTGRES_DB],
+    });
+
+    const openConnectionsValue =
+      openConnectionsResult.rows[0].opened_connections;
+    const secondQueryDuration = performance.now() - secondQueryTimer;
+
+    const thirdQueryTimer = performance.now();
+    const versionResult = await database.query("SHOW server_version;");
+    const versionResultValue = versionResult.rows[0].server_version;
+    const thirdQueryDuration = performance.now() - thirdQueryTimer;
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      database: {
+        status: "healthy",
+        max_connections: maxConnections,
+        opened_connections: openConnectionsValue,
+        latency: {
+          first_time_to_response: firstQueryDuration,
+          second_time_to_response: secondQueryDuration,
+          tird_time_to_response: thirdQueryDuration,
+        },
+        version: versionResultValue,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    response.status(200).json({
+      updated_at: updatedAt,
+      database: {
+        status: "unhealthy",
+      },
+    });
+  }
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,23 @@
-test("GET to /api/v1/status should ertun 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status/");
 
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  if (responseBody.database.status != "unhealthy") {
+    expect(responseBody.database).toBeDefined();
+    expect(responseBody.database.max_connections).toBeDefined();
+    expect(responseBody.database.opened_connections).toEqual(1);
+    expect(responseBody.database.latency.first_time_to_response).toBeDefined();
+    expect(responseBody.database.latency.second_time_to_response).toBeDefined();
+    expect(responseBody.database.latency.tird_time_to_response).toBeDefined();
+    expect(responseBody.database.version).toBeDefined();
+  }
 });
