3af25847737c7067ea789e695ae94a279f06f756
alexvenceslau <alexvcosta.com@gmail.com>
2025-02-08 16:19:50 -0300

    feat: add information and logo on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import styles from "./style.module.css";
+
 function Home() {
-  return <h1>Olá mundo!!! Que dia legal para programar..</h1>;
+  return (
+    <div>
+      <div className={styles.perfil}>
+        <Image src="/images/logo.png" width={200} height={200} alt="Perfil" />
+        <div className={styles.descricao}>
+          <h2>Alex Venceslau</h2>
+          <p>Desenvolvedor web desde 2009.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
